bf2b4d2dcf56e07e43dc3a8416f39d66e64e1a78
pistonvinegar <sarah@tune.com>
2015-11-04 19:19:37 -0800

    this better work

---
+++ README.md
@@ -0,0 +1,3 @@
+# TrulyOutrageous
+
+Here is some cool stuff about my Truely Outrageous Repo
---
+++ guessing-game/game.js
@@ -0,0 +1,13 @@
+var guess = prompt("The answer is 42");
+		if (guess == "42") {
+		alert("You entered the correct number.");
+	}
+	else {
+		if (guess !== "42") {
+			alert("You guessed wrong.");
+		}
+	}
+		if (guess !== "sunny" && "rainy") {
+			alert("It was sunny, yo.");
+	}
+
