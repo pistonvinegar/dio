baa09603209a24b452fff80bfd97386f506b6554
pistonvinegar <sarah@tune.com>
2015-11-05 20:44:01 -0800

    second draft

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # TrulyOutrageous
 
 Here is some cool stuff about my Truely Outrageous Repo
+This is where I write a joke or something.
+
+Roses are red, my name is Dave
+This poem makes no sense
+Microwave
--- guessing-game/game.js
+++ guessing-game/game.js
@@ -1,15 +1,26 @@
-var guess = prompt("The answer is 42");
-		if (guess == "42") {
-		alert("You entered the correct number.");
+var guess = prompt("What is black and white and red all over?");
+		if (guess == "newspaper" || "Newspaper") {
+		alert("You've heard this joke before. The answer is " + guess + ".");
 	}
-	else {
-		if (guess !== "42") {
-			alert("You guessed wrong.");
-		}
-	}
-		if (guess !== "sunny" && "rainy") {
-			alert("It was sunny, yo.");
-	}
+	else if 
+		(guess == "a dead penguin" || "dead penguin") {
+		alert("While technically true, " + guess + " is not correct.");
+		}  
+	else if 
+		(guess == "a dead nun" || "dead nun") {
+			alert( guess + "?! Gross!");
+	};
 
-	derp-de-derppppppp
+var guess2 = prompt("What is the greatest band of all time?");
+		if (guess2 == "Iron Maiden" || "iron maiden") {
+		alert("Yes!! Right the fuck on!! " + guess2 + " fucking rules.");
+	}
+	else if 
+		(guess2 == "Backstreet Boys" || "backstreet boys") {
+		alert("Not what I wanted, but yeah, " + guess2 + " want it that way.");
+		}  
+	else if 
+		(guess2 == "Nickelback" || "nickelback") {
+			alert("DISAGREE. " + guess2 + " is not a...I can't even...");
+	};
 
